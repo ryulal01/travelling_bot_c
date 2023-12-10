211b4ce209a7f38dcfa4571c35c927da61623507
Juliia <115921025+ryulal01@users.noreply.github.com>
2023-12-10 23:26:19 +0300

    Update bot.c

--- bot.c
+++ bot.c
@@ -4,26 +4,104 @@
 #include <jansson.h>
 
 int main() {
-    // инициализация бота
-    telebot_handler_t handle = telebot_create("BOT_TOKEN");
-
-    // получение списка последних обновлений
-    telebot_update_t *updates = telebot_get_updates(handle, -1, 0, 0, NULL);
-
-    // обработка каждого сообщения
-    for (int i = 0; i < telebot_array_length((void **) updates); i++) {
-        telebot_update_t *update = updates[i];
-        if (update->message != NULL && update->message->text != NULL) {
-            char *text = update->message->text;
-            // если сообщение содержит название города, отправляем фотографию
-            if (strlen(text) > 2) {
-                send_city_photo(handle, update->message->chat->id, text);
-            }
-        }
+    FILE *fp = fopen(".token", "r");
+    if (fp == NULL)
+    {
+        printf("Failed to open .token file\n");
+        return -1;
     }
 
-    // освобождение памяти
-    telebot_put_updates(updates, true);
+    char token[1024];
+    if (fscanf(fp, "%s", token) == 0)
+    {
+        printf("Failed to read token\n");
+        fclose(fp);
+        return -1;
+    }
+    printf("Token: %s\n", token);
+    fclose(fp);
+
+    telebot_handler_t handle;
+    if (telebot_create(&handle, token) != TELEBOT_ERROR_NONE)
+    {
+        printf("Telebot create failed\n");
+        return -1;
+    }
+
+  telebot_user_t me;
+    if (telebot_get_me(handle, &me) != TELEBOT_ERROR_NONE)
+    {
+        printf("Failed to get bot information\n");
+        telebot_destroy(handle);
+        return -1;
+    }
+
+    printf("ID: %d\n", me.id);
+    printf("First Name: %s\n", me.first_name);
+    printf("User Name: %s\n", me.username);
+
+    telebot_put_me(&me);
+
+    int index, count, offset = -1;
+    telebot_error_e ret;
+    telebot_message_t message;
+    telebot_update_type_e update_types[] = {TELEBOT_UPDATE_TYPE_MESSAGE};
+    
+    while (1)
+    {
+        telebot_update_t *updates;
+        ret = telebot_get_updates(handle, offset, 20, 0, update_types, 0, &updates, &count);
+        if (ret != TELEBOT_ERROR_NONE)
+            continue;
+        printf("Number of updates: %d\n", count);
+        
+        // обработка каждого сообщения
+        for (int i = 0; i < telebot_array_length((void **) updates); i++) {
+            telebot_update_t *update = updates[i];
+            if (update->message != NULL && update->message->text != NULL) {
+                char *text = update->message->text;
+                // если сообщение содержит название города, отправляем фотографию
+                if (strlen(text) > 2) {
+                    send_city_photo(handle, update->message->chat->id, text);
+                }
+            }
+        }
+        
+        for (index = 0; index < count; index++)
+        {
+            message = updates[index].message;
+            if (message.text)
+            {
+                printf("%s: %s \n", message.from->first_name, message.text);
+                if (strstr(message.text, "/dice"))
+                {
+                    telebot_send_dice(handle, message.chat->id, false, 0, "");
+                }
+                else
+                {
+                    char str[4096];
+                    if (strstr(message.text, "/start"))
+                    {
+                        snprintf(str, SIZE_OF_ARRAY(str), "Hello %s", message.from->first_name);
+                    }
+                    else
+                    {
+                        snprintf(str, SIZE_OF_ARRAY(str), "<i>%s</i>", message.text);
+                    }
+                    ret = telebot_send_message(handle, message.chat->id, str, "HTML", false, false, updates[index].message.message_id, "");
+                }
+                if (ret != TELEBOT_ERROR_NONE)
+                {
+                    printf("Failed to send message: %d \n", ret);
+                }
+            }
+            offset = updates[index].update_id + 1;
+        }
+        telebot_put_updates(updates, count);
+
+        sleep(1);
+    }
+
     telebot_destroy(handle);
     return 0;
 }
